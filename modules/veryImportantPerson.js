843a1ebbe6b9efae911b36fd9df20bf0e7cd8d98
Uwulinuwu <100658177+Uwulinuwu@users.noreply.github.com>
2022-03-01 11:44:00 +0800

    Update veryImportantPerson.js

--- modules/veryImportantPerson.js
+++ modules/veryImportantPerson.js
@@ -1,40 +1,72 @@
+
 "use strict";
-const schema = require('./schema.js');
+
+const schema = require('./core-schema.js');
+
 var viplevel;
+
 const DIYmode = (process.env.DIY) ? true : false;
+
 var viplevelCheckGroup = async function (groupID) {
+
     let rply = 0;
+
     if (!viplevel) {
-        viplevel = await schema.veryImportantPerson.find({}).catch(error => console.error('vip #8 mongoDB error: ', error.name, error.reson));
+
+        viplevel = await schema.veryImportantPerson.find({});
+
     }
+
     var findGP = viplevel.find(function (item) {
+
         return item.gpid == groupID && item.switch !== false;
+
     });
+
     rply = (findGP) ? findGP.level : 0;
+
     rply = (DIYmode) ? 5 : rply;
+
     return rply;
+
 }
+
 var viplevelCheckUser = async function (userid) {
-    let rply = 0;
+
+    let rply = '';
+
     if (!viplevel) {
-        viplevel = await schema.veryImportantPerson.find({}).catch(error => console.error('vip #20 mongoDB error: ', error.name, error.reson));
+
+        viplevel = await schema.veryImportantPerson.find({});
+
     }
+
     var findUser = viplevel.find(function (item) {
+
         return item.id == userid && item.switch !== false; // 
+
     });
+
     rply = (findUser) ? findUser.level : 0;
+
     rply = (DIYmode) ? 5 : rply;
+
     return rply;
+
 }
+
 async function renew() {
-    viplevel = await schema.veryImportantPerson.find({}).catch(error => console.error('vip #30 mongoDB error: ', error.name, error.reson));
+
+    viplevel = await schema.veryImportantPerson.find({});
+
 }
-
-
-setInterval(renew, 10 * 60 * 1000);
 
 module.exports = {
+
     viplevelCheckGroup: viplevelCheckGroup,
+
     viplevelCheckUser: viplevelCheckUser,
+
     renew: renew
+
 }
